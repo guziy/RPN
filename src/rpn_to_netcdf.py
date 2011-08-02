e95caf8b4591cbbc4c30da08a67860c834b5478d
Huziy Oleksandr <guziy.sasha@gmail.com>
2011-08-02 07:54:32 -0400

    more functions to manipulate data

---
+++ src/rpn_to_netcdf.py
@@ -0,0 +1,129 @@
+import os.path
+
+__author__="huziy"
+__date__ ="$Jul 25, 2011 4:56:03 PM$"
+
+
+import netCDF4 as nc
+import application_properties
+application_properties.set_current_directory()
+from rpn import RPN
+
+import os
+import numpy as np
+
+
+
+def extract_runoff_to_netcdf_file(filePath = 'data/pm1957090100_00589248p'):
+    surface_runoff_name = 'TRAF'
+    subsurface_runoff_name = 'TDRA'
+    level_tdra = 5
+    level_traf = 1
+
+
+    print filePath
+
+    #get data from the rpn file
+    rpnObj = RPN(filePath)
+
+    assert rpnObj.get_number_of_records() > 4, filePath
+    surfRunoff = rpnObj.get_first_record_for_name_and_level(surface_runoff_name, level = level_traf)
+    subSurfRunoff = rpnObj.get_first_record_for_name_and_level(subsurface_runoff_name, level = level_tdra)
+
+    nx, ny = surfRunoff.shape
+    ncFile = nc.Dataset(filePath + '.nc', 'w', format = 'NETCDF3_CLASSIC')
+    ncFile.createDimension('lon', nx)
+    ncFile.createDimension('lat', ny)
+
+    surfRunoffVar = ncFile.createVariable(surface_runoff_name, 'f', ('lon','lat'))
+    subSurfRunoffVar = ncFile.createVariable(subsurface_runoff_name, 'f', ('lon','lat'))
+
+    subSurfRunoffVar[:] = subSurfRunoff
+    surfRunoffVar[:] = surfRunoff
+    ncFile.forecast_hour = rpnObj.get_current_validity_date()
+    ncFile.close()
+
+    rpnObj.close()
+    os.remove(filePath)
+
+
+def extract_runoff_to_netcdf_folder(folder_path = 'data/CORDEX/Africa/Samples'):
+    for folder in os.listdir(folder_path):
+        monthFolderPath = os.path.join(folder_path, folder)
+
+        if not os.path.isdir(monthFolderPath):
+            continue
+        for fileName in os.listdir(monthFolderPath):
+
+            if fileName.endswith('.nc'):
+                continue
+            filePath = os.path.join(monthFolderPath, fileName)
+            extract_runoff_to_netcdf_file(filePath)
+
+
+def extract_sand_and_clay_from_rpn(rpn_path = 'data/geophys_africa'):
+    rpnFile = RPN(rpn_path)
+    sandField = rpnFile.get_3D_field('SAND')
+    clayField = rpnFile.get_3D_field('CLAY')
+    rpnFile.close()
+
+    ncFile = nc.Dataset('sand_clay_3d.nc', 'w', format = 'NETCDF3_CLASSIC')
+
+    
+    nx, ny = sandField[1].shape
+    nz = len(sandField)
+
+    print nx, ny, nz
+    print sandField[1].shape
+
+    sand = np.zeros((nx, ny, nz))
+    clay = np.zeros((nx, ny, nz))
+
+    for i in xrange(nz):
+        print i
+        sand[:, :, i] = sandField[ i + 1 ][:,:]
+        clay[:, :, i] = clayField[ i + 1 ][:,:]
+
+        print 'SAND:',i, np.min(sand[:, :, i]), np.max(sand[:, :, i])
+        print 'CLAY:',i, np.min(clay[:, :, i]), np.max(clay[:, :, i])
+
+    ncFile.createDimension('lon', nx)
+    ncFile.createDimension('lat', ny)
+    ncFile.createDimension('level', nz)
+
+    sandVar = ncFile.createVariable('SAND', 'f', ('lon','lat','level'))
+    clayVar = ncFile.createVariable('CLAY', 'f', ('lon','lat','level'))
+
+    sandVar[:] = sand
+    clayVar[:] = clay
+    ncFile.close()
+
+
+    pass
+
+def delete_files_with_nrecords(folder_path = 'data/CORDEX/Africa/Samples', n_records = 4):
+    for folder in os.listdir(folder_path):
+        monthFolderPath = os.path.join(folder_path, folder)
+        if not os.path.isdir(monthFolderPath):
+            continue
+
+        for fileName in os.listdir(monthFolderPath):
+            filePath = os.path.join(monthFolderPath, fileName)
+            rpnObj = RPN(filePath)
+            delete = False
+            if rpnObj.get_number_of_records()  == n_records:
+                delete = True
+            rpnObj.close()
+            if delete:
+                os.remove(filePath)
+                print 'removing %s' % filePath
+
+    pass
+
+
+
+if __name__ == "__main__":
+#    extract_sand_and_clay_from_rpn()
+#    delete_files_with_nrecords()
+    extract_runoff_to_netcdf_folder()
+    print "Hello World"
