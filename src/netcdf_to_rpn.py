71651765e360c0516a38bf99d68741a5b71684cb
Huziy Oleksandr <guziy.sasha@gmail.com>
2011-08-20 17:32:36 -0400

    modified netcdf_to_rpn related stuff

--- src/netcdf_to_rpn.py
+++ src/netcdf_to_rpn.py
@@ -3,9 +3,22 @@ __author__="huziy"
 __date__ ="$Aug 20, 2011 1:45:02 PM$"
 
 import netCDF4 as nc
+from rpn import RPN
 
 def convert(nc_path = 'data/directions_qc_dx0.1.nc'):
     ds = nc.Dataset(nc_path)
+
+    ncNameToRpnName = {'flow_direction_value': 'fldr', 'slope': 'slop', 
+                        'channel_length':'leng', 'accumulation_area':'facc',
+                        'lon': '>>', 'lat' : '^^'
+                      }
+    rObj = RPN('infocell.rpn' , mode = 'w')
+    for ncName, rpnName in ncNameToRpnName.iteritems():
+        data = ds.variables[ncName][:]
+        grid_type = 'E' if rpnName in ['>>', '^^'] else 'Z'
+        rObj.write_2D_field(name = rpnName, level = 1, data = data, grid_type = grid_type)
+    rObj.close()
+
 
     print ds.variables.keys()
 
