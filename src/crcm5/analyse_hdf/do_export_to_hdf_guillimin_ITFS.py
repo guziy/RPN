8c494c11243ff4afc6129a13de1e48ca9fcde8e6
huziy <guziy.sasha@gmail.com>
2014-10-27 12:37:00 -0400

    save modifications from guillimn

--- src/crcm5/analyse_hdf/do_export_to_hdf_guillimin_ITFS.py
+++ src/crcm5/analyse_hdf/do_export_to_hdf_guillimin_ITFS.py
@@ -110,27 +110,31 @@ def main():
     #hdf_file_path = "/sb/project/xgk-345-ab/huziy/PythonProjects/hdf_store/quebec_0.1_crcm5-hcd-r.hdf5"
 
     #On guillimin: crcm5-hcd-rl
-    data_folder = "/home/huziy/current_project/Output/quebec_0.1_crcm5-hcd-rl/all_files_in_one_dir"
-    hdf_file_path = "/home/huziy/current_project/PythonProjects/hdf_store/quebec_0.1_crcm5-hcd-rl.hdf5"
+    #data_folder = "/home/huziy/current_project/Output/quebec_0.1_crcm5-hcd-rl/all_files_in_one_dir"
+    #hdf_file_path = "/home/huziy/current_project/PythonProjects/hdf_store/quebec_0.1_crcm5-hcd-rl.hdf5"
 
     #Interflow spinup
     #data_folder = "/home/huziy/current_project/Output/quebec_0.1_crcm5-hcd-rl-intfl_spinup_ITFS/all_files_in_one_dir"
     #hdf_file_path = "/home/huziy/current_project/PythonProjects/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl_spinup_ITFS.hdf5"
 
     #Interflow 
-    #data_folder = "/home/huziy/current_project/Output/quebec_0.1_crcm5-hcd-rl-intfl_ITFS/all_files_in_one_dir"
-    #hdf_file_path = "/home/huziy/current_project/PythonProjects/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl_ITFS.hdf5"
+    ##data_folder = "/home/huziy/current_project/Output/quebec_0.1_crcm5-hcd-rl-intfl_ITFS/all_files_in_one_dir"
+    ##hdf_file_path = "/home/huziy/current_project/PythonProjects/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl_ITFS.hdf5"
+ 
+    #Interflow avoiding truncation 
+    data_folder = "/gs/project/ugh-612-aa/huziy/Output/quebec_0.1_crcm5-hcd-rl-intfl_ITFS_avoid_truncation/all_in_one_dir"
+    hdf_file_path = "/home/huziy/current_project/PythonProjects/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl_ITFS_avoid_truncation1979-1989.hdf5"
  
 
     dm = Crcm5ModelDataManager(samples_folder_path = data_folder, all_files_in_samples_folder=True)
     var_names = ["STFA", "PR", "TT", "AV", "AH", "TRAF", "TDRA", "I5", "I0", "I1", "I2", "IMAV", 
-                 "AS", "INTF", "QQ", "UU", "VV", "WW", "GZ", "HR", "HU", "CLDP", "LC", "LD", "AL"]
+                 "AS", "INTF", "QQ", "UU", "VV", "WW", "GZ", "HR", "HU", "CLDP", "LC", "LD", "AL", "L1"]
     #var_names = [ "I0", "I1", "I2", "IMAV"]
     #var_names = ["AS", ]
     #var_names = ["QQ", ]
     #var_names = ["INTF", ]
     #var_names = ["AL",]
-    var_names = ["L1", "IMAV", "I5", "I1", "I2"]
+    #var_names = ["L1", "IMAV", "I5", "I1", "I2"]
 
     dm.export_to_hdf(var_list = var_names, file_path= hdf_file_path, mode="a")
     export_static_fields_to_hdf(
