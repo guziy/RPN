5396806b210c6c7aa057dbeb8a3dbfb29cdb112d
huziy <guziy.sasha@gmail.com>
2014-12-15 08:43:28 -0500

    Analyse surface runoff difference based on swe tendencies

--- src/crcm5/analyse_hdf/interflow/analyze_intf-effect_during_melting.py
+++ src/crcm5/analyse_hdf/interflow/analyze_intf-effect_during_melting.py
@@ -7,7 +7,6 @@ import pandas as pd
 from datetime import datetime
 import numpy as np
 
-
 import matplotlib.pyplot as plt
 
 
@@ -26,7 +25,6 @@ def get_pandas_panel_sorted_for_year(year, the_table):
 
 def get_np_arr_sorted_for_year(year, the_table):
     return get_pandas_panel_sorted_for_year(year, the_table).values
-
 
 
 def main(intf_file="/skynet3_rech1/huziy/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl_ITFS.hdf5",
@@ -49,38 +47,33 @@ def main(intf_file="/skynet3_rech1/huziy/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl
         traf_table_intf = intf_handle.get_node("/TRAF")
         traf_table_no_intf = no_intf_handle.get_node("/TRAF")
 
-        # Get 2m air temperature in order to distinguish between rain and snow
+        # Get swe to determine when melting is happening
         swe_table_intf = intf_handle.get_node("/I5")
         swe_table_no_intf = no_intf_handle.get_node("/I5")
 
 
-        # Get 2m air temperature in order to distinguish between rain and snow
-        t2m_table_intf = intf_handle.get_node("/TT")
-        t2m_table_no_intf = no_intf_handle.get_node("/TT")
-
-        total_diff = 0
+        total_diff = None
         for year in range(start_year, end_year + 1):
             print "Processing year {}".format(year)
 
             # Get data for the simulation with interflow
             traf_intf = get_np_arr_sorted_for_year(year, traf_table_intf)
             swe_intf = get_np_arr_sorted_for_year(year, swe_table_intf)
-            t2m_intf = get_np_arr_sorted_for_year(year, t2m_table_intf)
 
             # Get data for the simulation with interflow
             traf_no_intf = get_np_arr_sorted_for_year(year, traf_table_no_intf)
             swe_no_intf = get_np_arr_sorted_for_year(year, swe_table_no_intf)
-            t2m_no_intf = get_np_arr_sorted_for_year(year, t2m_table_no_intf)
 
+            nt, nx, ny = swe_intf.shape
+            if total_diff is None:
+                total_diff = np.zeros(swe_intf.shape[1:])
 
-            place_and_time = (swe_intf > 0) & (t2m_intf > 0) & (swe_no_intf > 0) & (t2m_no_intf > 0) & \
-                             (traf_intf > 0) & (traf_no_intf > 0)
+            for t in range(1, nt):
+                place_where_melts = (swe_intf[t] < swe_intf[t - 1]) & (swe_no_intf[t] < swe_no_intf[t - 1])
 
-            the_diff = np.zeros(place_and_time.shape)
+                total_diff[place_where_melts] += traf_intf[place_where_melts] - traf_no_intf[place_where_melts]
 
-            the_diff[place_and_time] += traf_intf[place_and_time] - traf_no_intf[place_and_time]
-            total_diff += the_diff.sum(axis=0)
-            print "Number of relevant points: ", np.count_nonzero(place_and_time)
+            # print "Number of relevant points: ", np.count_nonzero(the_diff)
             print "Finished processing {}".format(year)
 
         # save the figure
@@ -89,10 +82,6 @@ def main(intf_file="/skynet3_rech1/huziy/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl
         plt.pcolormesh(total_diff.transpose())
         plt.colorbar()
         plt.savefig(os.path.join(img_folder, "traf_diff.png"))
-
-
-
-
 
     pass
 
@@ -104,5 +93,5 @@ if __name__ == '__main__':
     params = dict(
         intf_file="/skynet3_rech1/huziy/hdf_store/quebec_0.1_crcm5-hcd-rl-intfl_ITFS.hdf5",
         no_intf_file="/skynet3_rech1/huziy/hdf_store/quebec_0.1_crcm5-hcd-rl.hdf5",
-        start_year=1980, end_year=2010)
+        start_year=1980, end_year=1980)
     main(**params)
