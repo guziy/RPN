b85edfe55e1a2c03ac6418ef9235e132f8f82e80
huziy <guziy.sasha@gmail.com>
2014-12-16 19:38:29 -0500

    Use Agg backend by default

--- src/util/plot_utils.py
+++ src/util/plot_utils.py
@@ -1,12 +1,12 @@
 from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
-__author__="huziy"
-__date__ ="$Aug 17, 2011 3:49:28 PM$"
-
+__author__ = "huziy"
+__date__ = "$Aug 17, 2011 3:49:28 PM$"
 
 import numpy as np
 
-def draw_colorbar(fig, img, ax = None, boundaries = None, ticks = None):
+
+def draw_colorbar(fig, img, ax=None, boundaries=None, ticks=None):
     """
     Draw a nicely aligned colorbar to the axes ax
     fig - containing figure
@@ -14,7 +14,7 @@ def draw_colorbar(fig, img, ax = None, boundaries = None, ticks = None):
     """
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("right", "5%", pad="3%")
-    cb = fig.colorbar(img, cax = cax, boundaries = boundaries, ticks = ticks)
+    cb = fig.colorbar(img, cax=cax, boundaries=boundaries, ticks=ticks)
 
 
 def get_closest_tick_value(nticks, lower_limit):
@@ -37,24 +37,27 @@ def get_closest_tick_value(nticks, lower_limit):
         tick_value += d
 
 
-def apply_plot_params(font_size = 20, width_pt = 1000, aspect_ratio = 1, height_cm = None, width_cm = None):
+def apply_plot_params(font_size=20, width_pt=1000, aspect_ratio=1, height_cm=None, width_cm=None):
     """
     aspect_ratio = height / (width * golden_mean)
     """
+    import matplotlib
+    matplotlib.use("Agg")
+
     import pylab
     import math
 
     if width_pt is not None and width_cm is None:
-        inches_per_pt = 1.0 / 72.27               # Convert pt to inch
-        golden_mean = (math.sqrt(5.0) - 1.0) / 2.0       # Aesthetic ratio
-        fig_width = width_pt * inches_per_pt          # width in inches
-        fig_height = fig_width * golden_mean      # height in inches
+        inches_per_pt = 1.0 / 72.27  # Convert pt to inch
+        golden_mean = (math.sqrt(5.0) - 1.0) / 2.0  # Aesthetic ratio
+        fig_width = width_pt * inches_per_pt  # width in inches
+        fig_height = fig_width * golden_mean  # height in inches
         fig_size = [fig_width, aspect_ratio * fig_height]
     else:
         inches_per_cm = 1.0 / 2.54
         width_cm = 16.0 if width_cm is None else width_cm
         height_cm = 23.0 if height_cm is None else height_cm
-        fig_size = [width_cm * inches_per_cm, height_cm * inches_per_cm ]
+        fig_size = [width_cm * inches_per_cm, height_cm * inches_per_cm]
 
     params = {
         'axes.labelsize': font_size,
@@ -69,30 +72,30 @@ def apply_plot_params(font_size = 20, width_pt = 1000, aspect_ratio = 1, height_
     pylab.rcParams.update(params)
 
 
-def zoom_to_qc(plotter = None):
+def zoom_to_qc(plotter=None):
     ymin, ymax = plotter.ylim()
-    plotter.ylim(ymin + 0.05 * (ymax - ymin) , ymax * 0.25)
+    plotter.ylim(ymin + 0.05 * (ymax - ymin), ymax * 0.25)
 
     xmin, xmax = plotter.xlim()
-    plotter.xlim(xmin + (xmax - xmin) * 0.55, 0.72*xmax)
+    plotter.xlim(xmin + (xmax - xmin) * 0.55, 0.72 * xmax)
 
 
-def draw_meridians_and_parallels(the_basemap, step_degrees = 5.0, ax = None):
-    meridians = np.arange(-180,180, step_degrees)
-    parallels = np.arange(-90,90, step_degrees)
-    the_basemap.drawparallels(parallels,labels=[0,0,0,0],fontsize=16,linewidth=0.25, ax = ax)
-    the_basemap.drawmeridians(meridians,labels=[0,0,0,0],fontsize=16,linewidth=0.25, ax = ax)
+def draw_meridians_and_parallels(the_basemap, step_degrees=5.0, ax=None):
+    meridians = np.arange(-180, 180, step_degrees)
+    parallels = np.arange(-90, 90, step_degrees)
+    the_basemap.drawparallels(parallels, labels=[0, 0, 0, 0], fontsize=16, linewidth=0.25, ax=ax)
+    the_basemap.drawmeridians(meridians, labels=[0, 0, 0, 0], fontsize=16, linewidth=0.25, ax=ax)
+
 
 def get_ranges(x_interest, y_interest):
     """
     Get region of zoom for a given map
     """
-    x_min, x_max = np.min( x_interest ), np.max( x_interest )
-    y_min, y_max = np.min( y_interest ), np.max( y_interest )
+    x_min, x_max = np.min(x_interest), np.max(x_interest)
+    y_min, y_max = np.min(y_interest), np.max(y_interest)
     dx = 0.1 * ( x_max - x_min )
     dy = 0.1 * ( y_max - y_min )
     return x_min - dx, x_max + dx, y_min - dy, y_max + dy
-
 
 
 if __name__ == "__main__":
