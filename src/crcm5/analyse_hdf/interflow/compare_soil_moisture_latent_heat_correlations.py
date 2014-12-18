382bfc13c176eacd929514c663400dfd2d203ce3
huziy <guziy.sasha@gmail.com>
2014-12-18 11:42:04 -0500

    improve i0 and av correlation comparison plots

--- src/crcm5/analyse_hdf/interflow/compare_soil_moisture_latent_heat_correlations.py
+++ src/crcm5/analyse_hdf/interflow/compare_soil_moisture_latent_heat_correlations.py
@@ -1,4 +1,5 @@
 import matplotlib
+
 matplotlib.use("Agg")
 
 from collections import OrderedDict
@@ -9,10 +10,10 @@ import numpy as np
 
 __author__ = 'san'
 
-
 import matplotlib.pyplot as plt
 from calculate_interflow_correlations import calculate_correlation_field_for_climatology
 import crcm5.analyse_hdf.do_analysis_using_pytables as analysis
+from mpl_toolkits.basemap import maskoceans
 
 
 def plot_for_simulation(axis=None, sim_path="", cmap=None, cnorm=None,
@@ -41,9 +42,22 @@ def plot_for_simulation(axis=None, sim_path="", cmap=None, cnorm=None,
 
     corr = calculate_correlation_field_for_climatology(**params)
 
+    # convert longitudes to the [-180, 180] range
+    lons[lons > 180] -= 360
+    corr = maskoceans(lons, lats, corr)
+
     x, y = bm(lons, lats)
+
     im = bm.pcolormesh(x, y, corr, norm=cnorm, cmap=cmap, ax=axis)
     bm.drawcoastlines()
+    return im, corr
+
+
+def plot_correlation_diff(sim_label_to_corr, file_for_basemap="", ax=None, cnorm=None, cmap=None):
+    lons, lats, bm = analysis.get_basemap_from_hdf(file_path=file_for_basemap)
+    x, y = bm(lons, lats)
+    im = bm.pcolormesh(x, y, sim_label_to_corr.values()[1] - sim_label_to_corr.values()[0], cmap=cmap, cnorm=cnorm)
+    ax.set_title("({}) - ({})".format(sim_label_to_corr.keys()[1], sim_label_to_corr.keys()[0]))
     return im
 
 
@@ -56,18 +70,16 @@ def main():
 
     nsims = len(label_to_path)
     width_ratios = nsims * [1.0, ] + [0.05, ]
-    gs = GridSpec(1, nsims + 1, width_ratios=width_ratios)
+    gs = GridSpec(1, nsims + 3, width_ratios=width_ratios)
 
     cdelta = 0.2
     clevels = np.arange(-1.0, 1.0 + cdelta, cdelta)
     cnorm = BoundaryNorm(clevels, len(clevels) - 1)
     cmap = cm.get_cmap("RdBu_r", len(clevels) - 1)
 
-
     start_year = 1980
     end_year = 2010
     months = range(3, 6)
-
 
     keys = [k.replace(" ", "-") for k in label_to_path]
     keys.extend([str(i) for i in (start_year, end_year)])
@@ -76,17 +88,35 @@ def main():
     fig = plt.figure()
 
     im = None
-    for col, sim_path in enumerate(label_to_path.values()):
+    sim_label_to_corr = OrderedDict()
+    for col, sim_label, sim_path in enumerate(label_to_path.iteritems()):
         ax = fig.add_subplot(gs[0, col])
-        im = plot_for_simulation(axis=ax, sim_path=sim_path, cmap=cmap, cnorm=cnorm,
-                                 start_year=start_year, end_year=end_year, months=months)
+        im, corr = plot_for_simulation(axis=ax, sim_path=sim_path, cmap=cmap, cnorm=cnorm,
+                                       start_year=start_year, end_year=end_year, months=months)
+        ax.set_title(sim_label)
+        sim_label_to_corr[sim_label] = corr
 
     plt.colorbar(im, cax=fig.add_subplot(gs[0, nsims]), ticks=clevels)
+
+    # plot differences in correlation
+    clevels = np.arange(-2.0, 2.0 + cdelta, cdelta)
+    cnorm = BoundaryNorm(clevels, len(clevels) - 1)
+    cmap = cm.get_cmap("RdBu_r", len(clevels) - 1)
+
+    im = plot_correlation_diff(sim_label_to_corr, file_for_basemap=label_to_path.values()[0],
+                               ax=fig.add_subplot(gs[0, nsims + 1]), cnorm=cnorm, cmap=cmap)
+
+    plt.colorbar(im, cax=fig.add_subplot(gs[0, nsims + 2]), ticks=clevels)
+
 
     fig.savefig(img_path)
 
 
+
+
+
 if __name__ == '__main__':
     import application_properties
+
     application_properties.set_current_directory()
     main()
