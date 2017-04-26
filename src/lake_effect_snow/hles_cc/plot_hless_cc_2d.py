980581704960438ac438078ca7a37223307ed2a1
huziy <guziy.sasha@gmail.com>
2017-04-26 22:23:38 +0200

    debug

--- src/lake_effect_snow/hles_cc/plot_hless_cc_2d.py
+++ src/lake_effect_snow/hles_cc/plot_hless_cc_2d.py
@@ -93,6 +93,8 @@ def read_var_from_hles_alg_output(folder_path: Path, varname: str, start_year: i
                 print(ym_to_path[cy, cm], ds.variables[varname][:].shape)
                 y_to_fields[y].append(ds.variables[varname][:].squeeze())
 
+    print(y_to_fields)
+
     fields3d = np.ma.array([field for field in y_to_fields.values()])
     print(fields3d.shape)
 
