fix the mean calculation

src/lake_effect_snow/hles_cc/plot_hless_cc_2d.py
@@ -93,7 +93,7 @@ def read_var_from_hles_alg_output(folder_path: Path, varname: str, start_year: i
                 print(ym_to_path[cy, cm], ds.variables[varname][:].shape)
                 y_to_fields[y].append(ds.variables[varname][:].squeeze())
 
-        y_to_fields[y] = np.ma.mean([field for field in y_to_fields.values()], axis=0)
+        y_to_fields[y] = np.ma.mean(y_to_fields[y], axis=0)
 
     print(y_to_fields)
 