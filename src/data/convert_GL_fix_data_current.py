9810d7ca3600a13844aeaa69b7a8fa961d342e3f
huziy <guziy.sasha@gmail.com>
2019-08-07 20:03:04 -0400

    latest conversion options

--- src/data/convert_GL_fix_data_current.py
+++ src/data/convert_GL_fix_data_current.py
@@ -20,18 +20,19 @@ def parallel_conversion_entry():
               "STFL", "SWSL", "SWSR", "T5", "T9", "TDRA", "TJ", "TRAF", "UD", "VD"]
     """
 
-    fields = ["TT", "PR", "N3", "AV", "GZ", "P0", "SN", "N4", "TJ", "HU", "STFL", "TRAF"]
+    fields = ["TT", "PR", "N3", "AV", "GZ", "P0", "SN", "N4", "TJ", "HU", ]
 
     start_year = 1989
     end_year = 2010
 
     label_to_simpath = OrderedDict()
-    label_to_simpath[f"CanESM2_GL_{start_year}-{end_year}"] = "/scratch/huziy/Output/GL_CC_CanESM2_RCP85/coupled-GL-current_CanESM2/Samples"
+    # label_to_simpath[f"CanESM2_GL_{start_year}-{end_year}"] = "/scratch/huziy/Output/GL_CC_CanESM2_RCP85/coupled-GL-current_CanESM2/Samples"
+    label_to_simpath[f"CanESM2_GL_{start_year}-{end_year}"] = "/home/huziy/data/big1/Projects/HLES_GL_NEMO_CRCM5_CC/coupled-GL-current_CanESM2/Samples"
 
     input = [[start_year, end_year, fname, label_to_simpath] for fname in fields]
 
 
-    nprocs = min([multiprocessing.cpu_count(), 10, len(fields)])
+    nprocs = min([multiprocessing.cpu_count(), 1, len(fields)])
     p = Pool(processes=min(nprocs, len(fields)))
 
     # do the conversion in parallel
