daae5e9272d4d2ae3155136f43f3273e450a04d0
huziy <guziy.sasha@gmail.com>
2019-08-07 20:05:39 -0400

    start working on sownload_subset for daymet

---
+++ src/daymet/download_subset.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+import xarray
+
+def main():
+
+    beg_date = datetime(1989, 1, 1)
+    end_date = datetime(2011, 1, 1)
+
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
